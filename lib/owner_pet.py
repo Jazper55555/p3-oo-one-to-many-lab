ccff6e8a15931628030e4cdb44f691a33258597c
Jazper <jazpersaldana@yahoo.com>
2024-06-13 09:59:45 -0700

    one to many lab complete

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,35 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    
+    def __init__(self, name, pet_type, owner=None) -> None:
+        self.name = name
+        if pet_type not in self.PET_TYPES:
+            raise Exception('This is not an acceptable pet')
+        self.pet_type = pet_type
+        self.owner = owner
+        self.all.append(self)
 
 class Owner:
-    pass
+    
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def pets(self):
+        return Pet.all
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception('This is not an acceptable animal')
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        sorted_pets = sorted(Pet.all, key=lambda pet: pet.name)
+        return sorted_pets
+
+# Pet('Luca', 'dog')
+# Pet('Andrea', 'dog')
+# Owner('Jazz').get_sorted_pets()
+
+# breakpoint()
